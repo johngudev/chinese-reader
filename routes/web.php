7387fd1173dc456f5db964ddf1f3c15e3da1a8ec
John <johngucs@gmail.com>
2026-05-24 21:10:53 -0500

    dashboard defaults to characters screen

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return redirect('/generate');
+    return redirect('/characters');
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
