2cfb41b5d8f46ef03d9b8e9e267d76a86d736df6
John <johngucs@gmail.com>
2026-06-09 23:49:44 -0500

    add history link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,9 @@
                     <x-nav-link :href="route('characters')" :active="request()->routeIs('characters')">
                         {{ __('Characters') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('history')" :active="request()->routeIs('history')">
+                        {{ __('History') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -81,6 +84,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('characters')" :active="request()->routeIs('characters')">
                 {{ __('Characters') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('history')" :active="request()->routeIs('history')">
+                {{ __('History') }}
             </x-responsive-nav-link>
         </div>
 
