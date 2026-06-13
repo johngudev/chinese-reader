38da6d84ecd629514c8edf973b2cc50e0f01317f
John <johngucs@gmail.com>
2026-06-12 20:20:25 -0500

    add dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashb')" :active="request()->routeIs('dashb')">
+                    <x-nav-link :href="route('dash')" :active="request()->routeIs('dash')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('generate')" :active="request()->routeIs('generate')">
