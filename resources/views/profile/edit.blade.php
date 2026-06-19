515ef463a09101f70bd6d8a3fdb46f23131c86a6
John <johngucs@gmail.com>
2026-06-19 10:41:03 -0500

    add billing button to profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -19,6 +19,14 @@
                 </div>
             </div>
 
+            @if (auth()->user()->isPremium())
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.premium-billing-button')
+                </div>
+            </div>
+            @endif
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
---
+++ resources/views/profile/partials/premium-billing-button.blade.php
@@ -0,0 +1,17 @@
+<section class="space-y-6">
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Billing') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __('Manage your billing information and subscription plan in Stripe.') }}
+        </p>
+    </header>
+
+    <a href="{{ route('billing') }}">
+        <x-primary-button
+            x-data=""
+        >{{ __('Manage Billing') }}</x-primary-button>
+    </a>
+</section>
